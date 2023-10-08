1c8a13d3a4674db84c9ecadb6e83c4751655fee5
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-10-08 23:26:16 +0530

    Image Container added

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const img = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Assalaamu alaikkum, Amhar!!!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={img} style={styles.image} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +19,49 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, View, Image } from 'react-native';
+
+// const PlaceholderImage = require('./assets/images/background-image.png');
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.imageContainer}>
+//         <Image source={PlaceholderImage} style={styles.image} />
+//       </View>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#25292e',
+//     alignItems: 'center',
+//   },
+//   imageContainer: {
+//     flex: 1,
+//     paddingTop: 58,
+//   },
+//   image: {
+//     width: 320,
+//     height: 440,
+//     borderRadius: 18,
+//   },
+// });
