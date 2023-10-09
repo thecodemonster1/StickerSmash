535f903bf6fbf87ab196a83247068eb9a62c5aad
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-10-09 12:18:11 +0530

    update the app.js

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+
 
 const img = require('./assets/images/background-image.png');
 
@@ -7,7 +10,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={img} style={styles.image} />
+        <ImageViewer placeholderImageSource={img}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -32,36 +35,3 @@ const styles = StyleSheet.create({
   },
 });
 
-
-// import { StatusBar } from 'expo-status-bar';
-// import { StyleSheet, View, Image } from 'react-native';
-
-// const PlaceholderImage = require('./assets/images/background-image.png');
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <View style={styles.imageContainer}>
-//         <Image source={PlaceholderImage} style={styles.image} />
-//       </View>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#25292e',
-//     alignItems: 'center',
-//   },
-//   imageContainer: {
-//     flex: 1,
-//     paddingTop: 58,
-//   },
-//   image: {
-//     width: 320,
-//     height: 440,
-//     borderRadius: 18,
-//   },
-// });
