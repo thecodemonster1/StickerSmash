0e452ffb95b3ccf07065a87d6e459abca8c1bfdc
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-10-14 07:08:38 +0530

    update the app.js to saving image uri and show it

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
 
 import Button from './components/Button.js';
 import ImageViewer from './components/ImageViewer.js';
 import * as ImagePicker from 'expo-image-picker'; 
 
-const img = require('./assets/images/background-image.png');
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
@@ -15,7 +19,7 @@ export default function App() {
     });
 
     if (!result.canceled) {
-      console.log(result);
+      setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select any image.');
     }
@@ -24,7 +28,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={img}/>
+        <ImageViewer placeholderImageSource={PlaceholderImage} 
+        selectedImage={selectedImage}/>
       </View>
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a photo" onPress={pickImageAsync}/>
