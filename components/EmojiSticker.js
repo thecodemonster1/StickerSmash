403607c6539f1bd5f1bac9f0c5cba604013bac31
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-10-21 23:46:47 +0530

    a1

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,16 +1,42 @@
 import { View, Image } from 'react-native';
 import Animated from 'react-native-reanimated';
+import { TapGestureHandler } from 'react-native-gesture-handler';
+import {
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring,
+} from 'react-native-reanimated';
 
+const scaleImage = useSharedValue(imageSize);
 const AnimatedImage = Animated.createAnimatedComponent(Image);
+
+const onDoubleTap = useAnimatedGestureHandler({
+  onActive: () => {
+    if (scaleImage.value !== imageSize * 2) {
+      scaleImage.value = scaleImage.value * 2;
+    }
+  },
+});
+
+const imageStyle = useAnimatedStyle(() => {
+  return {
+    width: withSpring(scaleImage.value),
+    height: withSpring(scaleImage.value),
+  };
+});
+
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
   return (
     <View style={{ top: -350, right: -50 }}>
-      <AnimatedImage
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
+      <TapGestureHandler onGestureEvent={onDoubleTap} numberOfTaps={2}>
+        <AnimatedImage
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </TapGestureHandler>
     </View>
   );
 }
