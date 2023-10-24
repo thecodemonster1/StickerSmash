0e2acd3321e32c96fbf1baec7d8ff9773bcba78d
Muhammadu Amhar <75868135+thecodemonster1@users.noreply.github.com>
2023-10-24 08:11:50 +0530

    Capture a screenshot and save it

--- App.js
+++ App.js
@@ -55,7 +55,19 @@ export default function App() {
   };
 
   const onSaveImageAsync = async () => {
-    // we will implement this later
+    try{
+      const localUri = await captureRef(imageRef, {
+        height: 440,
+        quality: 1,
+      });
+
+      await MediaLibrary.saveToLibraryAsync(localUri);
+      if (localUri){
+        alert("Saved!");
+      }
+    } catch(e) {
+      console.log(e);
+    }
   };
 
   return (
